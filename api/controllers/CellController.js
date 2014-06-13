2fb60b79a56d52637f2c377f542aabbd21b1c1ce
Kevin Sivabalan <kevin@buzzbuzzhome.com>
2014-06-13 13:09:29 -0400

    user and cell

---
+++ api/controllers/CellController.js
@@ -0,0 +1,11 @@
+/**
+ * CellController
+ *
+ * @description :: Server-side logic for managing cells
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
